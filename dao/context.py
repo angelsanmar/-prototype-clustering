48c1713e6f44dac9a1c792cf4e2ecb8f7a048a8e
angelsanmar <103123144+angelsanmar@users.noreply.github.com>
2022-07-29 15:39:05 +0200

    Fix context.py dao

--- dao/context.py
+++ dao/context.py
@@ -4,15 +4,12 @@
 This module allows to import the communitu_module local library
 in al examples. To do that, it is necessary to include the next
 line in the import section of an example:
-
 from .context import community_detection
-
 '''
 
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import dao
-
+import community_module
